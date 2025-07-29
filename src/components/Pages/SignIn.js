f2bed2922e6388740bab0ec5d2bff57a134e616e
haseeb ahmad <haseebahmad0027@gmail.com>
2025-07-29 18:53:55 +0500

    updated sign in page to connect it with backend

--- src/components/Pages/SignIn.js
+++ src/components/Pages/SignIn.js
@@ -6,7 +6,7 @@ import Header from '../Header/Header';
 
 const SignIn = () => {
     const [username, setUsername] = useState('');
-    const [PIN, setPIN] = useState('');
+    const [pin, setPin] = useState('');
     const [error, setError] = useState('');
 
     const navigate = useNavigate()
@@ -18,19 +18,18 @@ const SignIn = () => {
         // fetching api from backend for login
         try {
 
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('http://localhost:5000/api/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ username, PIN }),
+                body: JSON.stringify({ username, pin }),
             });
 
             const data = await response.json()
-            if (response.ok) {
+            if (response.ok && data.redirectTo) {
                 console.log('Login successful:')
-                console.log(data.token)
-                navigate('/dashboard')
+                navigate(data.redirectTo)
             } else {
                 console.log(data.message);
                 setError(data.message || 'Invalid credentials');
@@ -91,13 +90,14 @@ const SignIn = () => {
                  id='PIN'
                  required
                  placeholder='PIN Code'
-                 value={PIN} 
-                 onChange={(e) => setPIN(e.target.value)}
+                 value={pin} 
+                 onChange={(e) => setPin(e.target.value)}
                  />
                  {error && <div className={classes["error-message"]}>{error}</div>}
             </div>
             <div>
                 <button type='submit'>Sign In</button>
+                {/* {error && <div style={{ color: 'red' }}>{error}</div>} */}
             </div>
         </form>
     </div>
